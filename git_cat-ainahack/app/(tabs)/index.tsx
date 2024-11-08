dabb9b23160b67c852d0c0b7e0ee5051c08a49a2
uripont-upf <oriol.pont01@estudiant.upf.edu>
2024-11-08 18:56:53 +0100

    first tts hosted model calling

--- git_cat-ainahack/app/(tabs)/index.tsx
+++ git_cat-ainahack/app/(tabs)/index.tsx
@@ -1,14 +1,30 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button } from 'react-native';
+import useHuggingFaceModel from '@/hooks/useHuggingFaceModel';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
+  const { response, loading, error, query } = useHuggingFaceModel();
+
+  const callHuggingFaceModel = () => {
+    console.log("Calling model");
+    query({
+      inputs: "Completa la següent frase: El gos és un animal ",
+      parameters: {
+        max_new_tokens: 150,
+      },
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Button title="Call Model" onPress={callHuggingFaceModel} />
+      {loading && <Text>Loading...</Text>}
+      {response && <Text>{JSON.stringify(response)}</Text>}
     </View>
   );
 }
---
+++ git_cat-ainahack/hooks/useHuggingFaceModel.ts
@@ -0,0 +1,42 @@
+
+import { useState } from 'react';
+
+const useHuggingFaceModel = () => {
+  const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<null | unknown>(null);
+
+  const query = async (data: any) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const apiUrl = process.env.EXPO_PUBLIC_HUGGING_FACE_API_URL;
+      if (!apiUrl) {
+        console.log("HUGGING_FACE_API_URL is not defined");
+        throw new Error('HUGGING_FACE_API_URL is not defined');
+      }
+      const result = await fetch(apiUrl, {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${process.env.EXPO_PUBLIC_HUGGING_FACE_TOKEN}`,
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify(data),
+      });
+      console.log("Sent request");
+      const json = await result.json();
+      console.log("Got response");
+      console.log(json);
+      setResponse(json);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { response, loading, error, query };
+};
+
+export default useHuggingFaceModel;
