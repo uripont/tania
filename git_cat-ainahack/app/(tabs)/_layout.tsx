0e498041bff007ef5bc2132cf2c1ecf092d7a40d
uripont-upf <oriol.pont01@estudiant.upf.edu>
2024-11-09 00:08:36 +0100

    adding tts + accent dropdown picker for test (later on on preferences screen). all models working

--- git_cat-ainahack/app/(tabs)/_layout.tsx
+++ git_cat-ainahack/app/(tabs)/_layout.tsx
@@ -48,9 +48,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="tts"
         options={{
-          title: 'Tab Two',
+          title: 'TTS',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
