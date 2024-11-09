afe4920b40765500faf637d77872ce9fd2d690c9
uripont-upf <oriol.pont01@estudiant.upf.edu>
2024-11-09 09:11:09 +0100

    preparing message array

--- git_cat-ainahack/state/stores/tania/taniaState.ts
+++ git_cat-ainahack/state/stores/tania/taniaState.ts
@@ -1,9 +1,10 @@
 export type TaniaPhase = 'FormSelection' | 'FormFilling';
-export type TaniaMode = 'Talking' | 'Listening' | 'Thinking1' | 'Thinking2';
+export type TaniaMode = 'Talking' | 'Listening' | 'Thinking1' | 'Thinking2' | 'Waiting';
 
 export interface TaniaState {
   phase: TaniaPhase;
   taniaMode: TaniaMode;
+
   voice: string;
   accent: string;
   type: string;
@@ -13,4 +14,15 @@ export interface TaniaState {
   setIsWaitingForUserInput: (isWaiting: boolean) => void;
   setVoice: (voice: string) => void;
   setAccent: (accent: string) => void;
+
+  messages: Message[];
+  addMessage: (message: Omit<Message, 'id' | 'timestamp'>) => void;
+  updateMessage: (id: string, content: string) => void;
+}
+
+export interface Message {
+  id: string;
+  timestamp: number;
+  type: 'system' | 'user' | 'editable-system';
+  content: string;
 }
--- git_cat-ainahack/state/stores/tania/taniaStore.ts
+++ git_cat-ainahack/state/stores/tania/taniaStore.ts
@@ -1,19 +1,20 @@
 import { create } from 'zustand';
 import { TaniaState, TaniaPhase, TaniaMode } from './taniaState';
 import { taniaStateLogger } from './taniaMiddleware';
+import { Message } from './taniaState';
 
 const initialPhase: TaniaPhase = 'FormSelection';
-const initialTaniaMode: TaniaMode = 'Listening';
+const initialTaniaMode: TaniaMode = 'Waiting';
 
 export const useTaniaStore = create<TaniaState>(
   taniaStateLogger<TaniaState>({ showOnlyChanges: true })(
-    (set: (partial: Partial<TaniaState>) => void) => ({
+    (set: (partial: Partial<TaniaState>) => void, get: () => TaniaState) => ({
       phase: initialPhase,
       taniaMode: initialTaniaMode,
       voice: 'elia',
       accent: 'central',
       type: 'text',
-      isWaitingForUserInput: false,
+      isWaitingForUserInput: true,
       setPhase: (phase: TaniaPhase) => set({ phase }),
 
       setTaniaMode: (mode: TaniaMode) => set({ taniaMode: mode }),
@@ -23,6 +24,27 @@ export const useTaniaStore = create<TaniaState>(
       setVoice: (voice: string) => set({ voice }),
 
       setAccent: (accent: string) => set({ accent }),
+
+      messages: [],
+
+      addMessage: (message: Omit<Message, 'id' | 'timestamp'>) =>
+        set({
+          messages: [
+            ...get().messages,
+            {
+              ...message,
+              id: crypto.randomUUID(),
+              timestamp: Date.now(),
+            },
+          ],
+        }),
+
+      updateMessage: (id: string, content: string) =>
+        set({
+          messages: get().messages.map((msg) =>
+            msg.id === id ? { ...msg, content } : msg
+          ),
+        }),
     })
   )
 );
