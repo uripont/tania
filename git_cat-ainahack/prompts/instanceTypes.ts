3091f96000f98eb6c4d4adb0aacb6fe7977dd0dd
uripont-upf <oriol.pont01@estudiant.upf.edu>
2024-11-09 02:56:44 +0100

    extended prompt for stage 1. still not tied to state, but working effectively. ready to merge to main.

--- git_cat-ainahack/prompts/instanceTypes.ts
+++ git_cat-ainahack/prompts/instanceTypes.ts
@@ -1,10 +1,24 @@
 export const TIPUS_INSTANCIES = `TIPUS D’INSTÀNCIES:
-1- INSTÀNCIA GENÈRICA.
-2- INSTÀNCIA BECA MENJADOR.
-3- INSTÀNCIA DUPLICAT DE CERTIFICAT.`;
+1- INSTÀNCIA GENÈRICA. Descripció: Instància per a sol·licitar informació general o ajuda amb tràmits administratius.
+2- INSTÀNCIA BECA MENJADOR. Descripció: Instància per sol·licitar ajudes de menjador per a estudiants.
+3- INSTÀNCIA DUPLICAT DE CERTIFICAT. Descripció: Instància per sol·licitar un còpies o duplicats de documents oficials, habitualment perduts.
+4- INFORME D'ACCIDENT DE TRÁNSIT DE LA GUARDIA URBANA. Descripció: Instància per a documentar l'ocurrència de sinistres de trànsit a l'autoritat'.
+5- CERTIFICAT DE PAGAMENT DE MULTES, IMPOSTOS, TAXES I PREUS PÚBLICS. Descripció: Instància per a sol·licitar una acreditació que et permeti pagar diferents tràmits públics.
+6- BONIFICACIÓ DE L'IMPOST SOBRE  BÉNS IMMOBLES. Descripció: Instància per a sol·licitar una bonificació de la quota de l’Impost sobre Béns Immobles (IBI).
+7- CANVI D'ADREÇA FISCAL. Descripció: Instància per a comunicar un canvi de domicili de contribuents donats d'alta a les bases de dades de tributs i multes de l'Institut Municipal d'Hisenda.
+8- SUSPENSIÓ DE L'EXECUCIÓ DE SANCIONS PER INFRACCIONS DE TRÀNSIT. Descripció: Instància per a sol·licitar la suspensió de l’execució de sancions per a les normes de circulació per infraccions lleus o greus que no comportin suspensió del permís o llicència de conducció.
+9- ALTA PER NAIXEMENT AL PADRÓ MUNICIPAL D'HABITANTS. Descripció: Instància per a sol·licitar l’alta al padró municipal del vostre nadó.
+10- BAIXA DE LA LLICÈNCIA DE TERRASSES. Descripció: Instància per a sol·licitar la baixa del permís d'instal·lació de terrasa ubicada en les places, carrers i espais d'ús públic.`;
 
 export const LLISTA_INSTANCIES = [
     "INSTÀNCIA GENÈRICA",
     "INSTÀNCIA BECA MENJADOR",
-    "INSTÀNCIA DUPLICAT DE CERTIFICAT"
+    "INSTÀNCIA DUPLICAT DE CERTIFICAT",
+    "INSTÀNCIA D'ACCIDENT DE TRÀNSIT DE LA GUARDIA URBANA",
+    "CERTIFICAT DE PAGAMENT DE MULTES, IMPOSTOS, TAXES I PREUS PÚBLICS",
+    "BONIFICACIÓ DE L'IMPOST SOBRE  BÉNS IMMOBLES",
+    "CANVI D'ADREÇA FISCAL",
+    "SUSPENSIÓ DE L'EXECUCIÓ DE SANCIONS PER INFRACCIONS DE TRÀNSIT",
+    "ALTA PER NAIXEMENT AL PADRÓ MUNICIPAL D'HABITANTS",
+    "BAIXA DE LA LLICÈNCIA DE TERRASSES",
 ]
